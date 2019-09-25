066dcacc90be43c3bc1f46b388eca2577963dd9e
Ishan Das <10532854@mydbs.ie>
2019-09-25 10:50:20 +0100

    sep25 end

--- PrgAnalytics/PrgAnalytics.py
+++ PrgAnalytics/PrgAnalytics.py
@@ -3,3 +3,17 @@ print("test")
 firstName = input ("Enter your first name ")
 lastName = input("Enter your last name ")
 print("Hi "+firstName+" "+lastName)
+
+#datatypes
+#-numeric and string
+#-numeric-- integer and decimals/floats
+
+#converting anything to string can be done with str()
+#operators- arthimetic --
+#           adding multiplyin divide sub modulus etc stuff
+#convert whole number string to integer with int()
+#float for float()
+
+hoursWorked = input("enter number of hours worked")
+wage = inpute("enter wage")
+print("price paid ", int(hoursWorked)* float(wage))
